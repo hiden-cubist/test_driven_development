d513c4eb21715ebfaa5cd865e60f127f670e3a0c
hiden-cubist <hiden205cubist@gmail.com>
2020-02-12 15:08:43 +0900

    implement section 4-5

--- src/test/money/MoneyTest.java
+++ src/test/money/MoneyTest.java
@@ -8,15 +8,20 @@ class MoneyTest {
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(10, product.amount);
-        product = five.times(3);
-        assertEquals(15, product.amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
     void testEquality() {
         assertEquals(new Dollar(5), new Dollar(5));
         assertNotEquals(new Dollar(5), new Dollar(6));
+    }
+
+    @Test
+    void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
     }
 }
